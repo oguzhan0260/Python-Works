00bffdff9a654f05b670aa62b1a6da437b5d1d93
Oğuzhan Sürmeli <78919149+oguzhansurmeli@users.noreply.github.com>
2023-01-27 20:00:01 +0300

    Add files via upload

---
+++ En_büyük_sayıyı_bul.py
@@ -0,0 +1,12 @@
+""" Problem 2
+Kullanıcıdan 3 tane sayı alın ve en büyük sayıyı ekrana yazdırın.
+
+"""
+print("En büyük sayıyı ekrana bastıran program.")
+list=[]
+list.append(float(input("1.Sayı:")))
+list.append(float(input("2.Sayı:")))
+list.append(float(input("3.Sayı:")))
+list.sort
+print(list)
+print(list[0])
